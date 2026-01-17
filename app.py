f2568e92970aedf7785a75c4094bb893916b4554
damusama <170484560+damusama@users.noreply.github.com>
2026-01-18 05:51:57 +0900

    render用

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route("/")
 def index():
   return render_template("index.html")
 
+@app.route("/sounds/<path:filename>")
+def sounds(filename):
+  return send_from_directory("sounds", filename)
+
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+  app.run(host="0.0.0.0", port=port, debug=False)
